7e20aeb6b9f64e58655a78aee3c131de7c8c59c7
Monish1780 <monishputtu1780@gmail.com>
2025-07-23 22:53:27 +0530

    meetings edit & delete

--- src/modules/meetings/components/update-meeting-dialog.tsx
+++ src/modules/meetings/components/update-meeting-dialog.tsx
@@ -1,31 +1,34 @@
 import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { MeetingForm } from "./meeting-form";
 import { useRouter } from "next/navigation";
+import { MeetingsGetOne } from "../types";
 
 interface NewMeetingDialogProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  initialValues: MeetingsGetOne;
 }
 
-export const NewMeetingDialog = ({
+export const UpdateMeetingDialog = ({
   open,
   onOpenChange,
+  initialValues,
 }: NewMeetingDialogProps) => {
   const router = useRouter();
 
   return (
     <ResponsiveDialog
-      title="New Meeting"
-      description="Create a new Meeting"
+      title="Edit Meeting"
+      description="Edit the meeting details"
       open={open}
       onOpenChange={onOpenChange}
     >
       <MeetingForm
-        onSuccess={(id) => {
+        onSuccess={() => {
           onOpenChange(false);
-          router.push(`/meetings/${id}`);
         }}
         onCancel={() => onOpenChange(false)}
+        initailValues={initialValues}
       />
     </ResponsiveDialog>
   );
---
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { MeetingIdViewHeader } from "../../components/meeting-id-view-header";
+import { useRouter } from "next/navigation";
+import { useConfirm } from "@/hooks/use-confirm";
+import { UpdateMeetingDialog } from "../../components/update-meeting-dialog";
+import { useState } from "react";
+
+interface Props {
+  meetingId: string;
+}
+
+export const MeetingIdView = ({ meetingId }: Props) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const [UpdateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
+
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    "The following action will remove this meeting"
+  );
+
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+
+  const removeMeeting = useMutation(
+    trpc.meetings.remove.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.meetings.getMany.queryOptions({}));
+        router.push("/meetings");
+      },
+    })
+  );
+
+  const handleRemoveMeeting = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeMeeting.mutateAsync({ id: meetingId });
+  };
+
+  return (
+    <>
+      <RemoveConfirmation />
+      <UpdateMeetingDialog
+        open={UpdateMeetingDialogOpen}
+        onOpenChange={setUpdateMeetingDialogOpen}
+        initialValues={data}
+      />
+      <div className="slex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <MeetingIdViewHeader
+          meetingId={meetingId}
+          meetingName={data.name}
+          onEdit={() => setUpdateMeetingDialogOpen(true)}
+          onRemove={handleRemoveMeeting}
+        />
+        MeetingIdView
+      </div>
+    </>
+  );
+};
+
+export const MeetingsIdViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="This may take a few seconds..."
+    />
+  );
+};
+
+export const MeetingsIdViewError = () => {
+  return (
+    <ErrorState
+      title="Error loading meetings"
+      description="Something went wrong"
+    />
+  );
+};
